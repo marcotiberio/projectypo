da4660a36664627a7f761a4b4bf1eb5342ea32f4
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-25 17:30:14 +0200

    revert

--- result.php
+++ result.php
@@ -16,7 +16,7 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content-result', 'page' );
+			get_template_part( 'template-parts/content', 'page' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
@@ -25,6 +25,9 @@ get_header();
 
 		endwhile; // End of the loop.
 		?>
+
+		<div class="font-result"><?php the_field('font-result'); ?></div>
+		<div class="font-description"><?php the_field('font-description'); ?></div>
 
 	</main><!-- #main -->
 
