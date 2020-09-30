a3e836e2528b10797b04f7ed8fea01d25015071d
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-30 11:30:22 +0200

    script js

--- functions.php
+++ functions.php
@@ -146,7 +146,11 @@ function projectypo_scripts() {
 	wp_enqueue_style( 'projectypo-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'projectypo-style', 'rtl', 'replace' );
 
+	wp_deregister_script( 'jquery' );
+	wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js', array(), null, true);
+
 	wp_enqueue_script( 'projectypo-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'projectypo-script', get_template_directory_uri() . '/js/script.js', array( 'jquery' ), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
