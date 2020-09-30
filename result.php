b6dac52901e995cb38b1a2a6abd718d8014c6109
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-30 14:22:27 +0200

    test acf

--- result.php
+++ result.php
@@ -18,16 +18,11 @@ get_header();
 
 			get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
 		endwhile; // End of the loop.
 		?>
 
-		<div class="font-result"><?php the_field('font-result'); ?></div>
-		<div class="font-description"><?php the_field('font-description'); ?></div>
+		<div class="font-result"><?php the_field('font_result'); ?></div>
+		<div class="font-description"><?php the_field('font_description'); ?></div>
 
 	</main><!-- #main -->
 
