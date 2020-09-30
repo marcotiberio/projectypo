337ead48d9c65e542cf593f0f01f939d69cf36a3
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-30 11:36:31 +0200

    test cursor

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-$(window).on('mousemove', function (e) {
-    var lSide = $(this).height() / 2;
-    var cursorImg = e.pageX < lSide ? 'cursorU' : 'cursorB';
-    $('body').css({
-        "cursor": "url('assets/" + cursorImg + ".png'), auto"
+$(document).ready(function () {
+    $(window).on('mousemove', function (e) {
+        var lSide = $(this).height() / 2;
+        var cursorImg = e.pageX < lSide ? 'cursorU' : 'cursorB';
+        $('body').css({
+            "cursor": "url('assets/" + cursorImg + ".png'), auto"
+        });
+        console.log(cursorImg) // Just to see check it ;)
     });
-    console.log(cursorImg) // Just to see check it ;)
-})
+});
