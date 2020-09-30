0eacd414101f67a3445ba21b3503510479ade085
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-30 14:27:08 +0200

    no loop result page

--- result.php
+++ result.php
@@ -11,15 +11,6 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-		endwhile; // End of the loop.
-		?>
 		<div class="results">
 			<div class="font-result"><?php the_field('font_result'); ?></div>
 			<div class="font-description"><?php the_field('font_description'); ?></div>
